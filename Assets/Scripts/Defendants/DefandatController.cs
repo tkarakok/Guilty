2d1cca52f75f6ce75c4198ec53eb33c2884473a9
Tunahan <karakoktunahan78@gmail.com>
2022-03-20 01:56:05 +0300

    New wpath added

--- Assets/Scripts/Defendants/DefandatController.cs
+++ Assets/Scripts/Defendants/DefandatController.cs
@@ -1,12 +1,13 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using PathCreation.Examples;
 
 public class DefandatController : MonoBehaviour
 {
     public Sprite signature;
-    
     private Animator _animator;
 
     private void Start() {
@@ -15,28 +16,29 @@ public class DefandatController : MonoBehaviour
 
     public void Guilty(){
         _animator.SetBool("Idle",false);
-        transform.DORotate(new Vector3(0,-90,0),.5f).OnComplete(() => {
-            transform.DOMoveX(5,3);
-            gameObject.SetActive(false);
-        });
+        gameObject.GetComponent<PathFollower>().pathCreator = GamaManager.Instance.guiltyPath;
+        gameObject.GetComponent<PathFollower>().endOfPathInstruction = PathCreation.EndOfPathInstruction.Loop;
     }
 
     public void NonGuilty(){
         _animator.SetBool("Idle",false);
-        transform.DORotate(new Vector3(0,90,0),.5f).OnComplete(() => {
-            transform.DOMoveX(-5,3);
-            gameObject.SetActive(false);
-        });
+        gameObject.GetComponent<PathFollower>().pathCreator = GamaManager.Instance.nonGuiltyPath;
+        gameObject.GetComponent<PathFollower>().endOfPathInstruction = PathCreation.EndOfPathInstruction.Loop;
     }
 
     
 
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Stop"))
-        {
+        {   
+            gameObject.layer = 6;
             UIManager.Instance.OpenFilesPanel(true);
             UIManager.Instance.UpdateSignature(gameObject);
             _animator.SetBool("Idle",true);
+        }
+        else if (other.CompareTag("Destroy"))
+        {
+            gameObject.SetActive(false);
         }
     }
 }
--- Assets/Scripts/Game/GamaManager.cs
+++ Assets/Scripts/Game/GamaManager.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using PathCreation;
 public class GamaManager : Singleton<GamaManager>
 {
     public LevelSettings levelSettings;
     public Transform guiltyPoint,nonGuiltyPoint;
-
+    public PathCreator guiltyPath,nonGuiltyPath;
     #region Private Fields
     private int _totalDefendat;
     private int _complateDefendats = 0;
